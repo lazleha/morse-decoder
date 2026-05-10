dc2fa4d23d1e40b5bf2c4cb216675407a7c4e39c
lazleha <fojcsog@gmail.com>
2026-05-10 16:12:05 +0200

    feat: implement morse decoder

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-/*
 const MORSE_TABLE = {
   '.-': 'a',
   '-...': 'b',
@@ -37,8 +36,24 @@ const MORSE_TABLE = {
   '----.': '9',
   '-----': '0',
 };
-*/
 
-module.exports = function decode(/* expr */) {
-  throw new Error('Not implemented');
-};
+function decode(expr) {
+  const result = [];
+  for (let i = 0; i < expr.length; i += 10) {
+    const chunk = expr.slice(i, i + 10);
+    if (chunk === '**********') {
+      result.push(' ');
+    } else {
+      let morseChar = '';
+      for (let j = 0; j < chunk.length; j += 2) {
+        const pair = chunk.slice(j, j + 2);
+        if (pair === '10') morseChar += '.';
+        if (pair === '11') morseChar += '-';
+      }
+      result.push(MORSE_TABLE[morseChar]);
+    }
+  }
+  return result.join('');
+}
+
+module.exports = decode;
